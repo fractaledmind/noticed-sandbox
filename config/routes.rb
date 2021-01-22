81e7c3cb82111319aea956ce73d7ca053a719647
fractaledmind <stephen.margheim@gmail.com>
2021-01-22 18:33:07 +0100

    Add Notifications#index route and make it root

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
+  resources :notifications, only: [:index]
   resources :notification_requests
   resources :notification_channels
   devise_for :users
-  root to: "notification_channels#new"
+  root to: "notifications#index"
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
