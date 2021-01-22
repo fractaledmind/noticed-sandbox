5247b32cb6ab23ac75c77e30f5f9943d89351023
fractaledmind <stephen.margheim@gmail.com>
2021-01-22 17:25:47 +0100

    Tweak setup for NotificationChannel

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :notification_channels
   devise_for :users
-  root to: "home#index"
+  root to: "notification_channels#new"
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
