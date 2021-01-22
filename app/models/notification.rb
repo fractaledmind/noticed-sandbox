e93048c5e3841c0a05940959a1e61a0e321790a0
fractaledmind <stephen.margheim@gmail.com>
2021-01-22 18:34:36 +0100

    Create a Notification when a NotificationRequest is created

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,4 +1,9 @@
 class Notification < ApplicationRecord
   include Noticed::Model
   belongs_to :recipient, polymorphic: true
+
+  def to_notification
+    name = type.remove("Notifier")
+    NotificationChannel.find_by(name: name).to_noticed_class.with(params)
+  end
 end
--- app/models/notification_request.rb
+++ app/models/notification_request.rb
@@ -1,3 +1,12 @@
 class NotificationRequest < ApplicationRecord
   belongs_to :channel, class_name: "NotificationChannel"
+
+  serialize :params, JSON
+
+  after_create_commit :deliver_notification
+
+  def deliver_notification
+    data = params.is_a?(String) ? JSON.parse(params) : params
+    channel.to_noticed_class.with(data).deliver(User.all)
+  end
 end
