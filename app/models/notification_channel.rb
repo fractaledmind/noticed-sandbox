dc3a5f2a9b10414be0b641380c1e6d8ba89dcd96
fractaledmind <stephen.margheim@gmail.com>
2021-01-22 18:34:02 +0100

    Dynamically create a Noticed class for a NotificationChannel

--- app/models/notification_channel.rb
+++ app/models/notification_channel.rb
@@ -1,2 +1,21 @@
 class NotificationChannel < ApplicationRecord
+  def to_noticed_class
+    channel_subject_template = subject_template
+    channel_body_template = body_template
+
+    klass = Class.new(Noticed::Base) do
+      deliver_by :database
+
+      define_method :subject do
+        channel_subject_template % params.symbolize_keys
+      end
+
+      define_method :body do
+        channel_body_template % params.symbolize_keys
+      end
+    end
+
+    Object.const_set "#{name}Notifier", klass
+  end
 end
+
